6b97f87d3c166951c7811072d6bf3fe6359fdd28
Carina Carvalho Dias <carinadias.dev@gmail.com>
2026-03-24 06:40:06 -0300

    Sequência_de_ Fibonacci.py
    O programa pergunta ao usuário quantos termos ele quer mostrar. em sequência ele mostra os termos, até, que o usuário não queira mostrar mais termos, digitando, 0 para sair do programa.

---
+++ Mundo02Python/exercicio063.py
@@ -0,0 +1,17 @@
+# Escreva um programa que leia um número n inteiro qualquer e mostre na tela os
+#n primeiros elementos de uma sequência de fibonacci. ex: 0->1->1->2->3->5->8
+
+print('Sequência de Fibonacci')
+print('='*40)
+n = int(input('Informe quantos termos você quer mostrar: '))
+t1 = 0
+t2 = 1
+print(f'{t1}-> {t2}-> ', end='')
+c = 3
+while c <= n:
+    t3 = t1 + t2
+    print(f'{t3}-> ',end='')
+    t1 = t2
+    t2 = t3
+    c += 1
+print('FIM')
